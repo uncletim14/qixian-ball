c0928a12d7616407eccb81fd42a119d09e227d8f
uncletim14 <saiigh587@gmail.com>
2026-09-23 22:21:59 +0800

    更新網站BUG

--- app/page.js
+++ app/page.js
@@ -277,8 +277,6 @@ export default function Home() {
   }, [selectedSession]);
 
   useEffect(() => {
-    setAdminPin('');
-    setIsAdminAuthenticated(false);
     setCheckInName('');
     setCheckInPassword('');
   }, [isCheckInMode]);
